def4c8768c7fc6e17c17ade264562e82e719b61e
Christopher Alcock <christopheralcock@Christophers-MacBook-Pro-2.local>
2020-05-02 19:49:25 +0100

    sort of works

---
+++ Assets/Chaser.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chaser : MonoBehaviour
+{
+
+    public int speedMultiplier = 200;
+
+    private void Update()
+    {
+    }
+
+
+
+    public void Jump(Vector2 mousePosition)
+    {
+        Vector2 currentPosition = new Vector2(transform.position.x, transform.position.y);
+        Debug.Log("jump");
+        Vector2 direction = currentPosition - mousePosition;
+        GetComponent<Rigidbody2D>().AddForce(direction * speedMultiplier);
+    }
+}
---
+++ Assets/Flinger.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flinger : MonoBehaviour
+{
+    private Chaser chaser;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        chaser = FindObjectOfType<Chaser>();
+        Debug.Log("found chaser");
+        Debug.Log(chaser);
+    }
+
+    private void OnMouseDown()
+    {
+        Debug.Log("that mouse is down");
+    }
+
+    private void OnMouseUp()
+    {
+        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        chaser.Jump(mousePosition);
+        Debug.Log("flinger onmouseup");
+    }
+}
