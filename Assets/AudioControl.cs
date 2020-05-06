e820bab827896ef3051601af7c6f3c574a9a523a
Christopher Alcock <christopheralcock@Christophers-MacBook-Pro-2.local>
2020-05-06 14:52:05 +0100

    very basic music. sounds good on unity but sucks in browser as effects don't seem to work

---
+++ Assets/AudioControl.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioControl : MonoBehaviour
+{
+    public float volume = 0.13f;
+
+    void Awake()
+    {
+        AudioListener.volume = this.volume;
+    }
+
+
+}
