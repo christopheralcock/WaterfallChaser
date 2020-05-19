da0575d5be757d683308ccda4857369a297840dd
Christopher Alcock <christopheralcock@Christophers-MacBook-Pro-2.local>
2020-05-19 20:10:01 +0100

    two levels to teach the basic mechanics

--- Assets/CanvasController.cs
+++ Assets/CanvasController.cs
@@ -8,18 +8,61 @@ public class CanvasController : MonoBehaviour
 
     public LevelController levelController;
     public Text completionMessage;
+    bool completionMessagePresent = false;
     public Text scrollInstructions;
+    bool scrollInstructionsPresent = false;
     public Text flingInstructions;
+    bool flingInstructionsPresent = false;
     public NextLevelButton nextLevelButton;
     public bool levelComplete = false;
+    public string completionMessageString = @"
+CONGRATULATIONS!
+
+LEVEL COMPLETE!
+
+THANKS A LOT FOR PLAYING
+
+GIVE CHRIS SOME FEEDBACK
+@chrisalcockdev";
 
 
     void Awake()
     {
         this.levelController = FindObjectOfType<LevelController>();
-        this.completionMessage = GameObject.FindGameObjectWithTag("CompletionMessage").GetComponent<Text>();
         this.nextLevelButton = FindObjectOfType<NextLevelButton>();
         this.nextLevelButton.gameObject.SetActive(false);
+
+        try
+        {
+            this.completionMessage = GameObject.FindGameObjectWithTag("CompletionMessage").GetComponent<Text>();
+            this.completionMessagePresent = true;
+        }
+        catch (System.Exception ex)
+        {
+            Debug.Log("no completion message, and that is fine");
+            Debug.Log(ex.ToString());
+        }
+
+        try
+        {
+            this.scrollInstructions = FindObjectOfType<ScrollInstructions>().GetComponent<Text>();
+            this.scrollInstructionsPresent = true;
+        }
+        catch (System.Exception ex) {
+            Debug.Log("no scroll instructions, and that is fine");
+            Debug.Log(ex.ToString());
+        }
+
+        try
+        {
+            this.flingInstructions = FindObjectOfType<FlingInstructions>().GetComponent<Text>();
+            this.flingInstructionsPresent = true;
+        }
+        catch (System.Exception ex)
+        {
+            Debug.Log("no fling instructions, and that is fine");
+            Debug.Log(ex.ToString());
+        }
     }
 
     // Update is called once per frame
@@ -27,27 +70,23 @@ public class CanvasController : MonoBehaviour
     {
         if (levelController.levelComplete && !this.levelComplete)
         {
-//            completionMessage.text = @"
-//CONGRATULATIONS!
-
-//LEVEL COMPLETE!
-
-//THANKS A LOT FOR PLAYING
-
-//GIVE CHRIS SOME FEEDBACK
-//@chrisalcockdev";
             nextLevelButton.gameObject.SetActive(true);
             this.levelComplete = true;
+            if (completionMessagePresent)
+            {
+                completionMessage.text = completionMessageString;
+            }
         }
 
-        if (levelController.firstFlingHappened)
+        if (levelController.firstFlingHappened && this.flingInstructionsPresent)
         {
             flingInstructions.text = null;
         }
 
-        if (levelController.firstScrollHappened)
+        if (levelController.firstScrollHappened && this.scrollInstructionsPresent)
         {
             scrollInstructions.text = null;
+            scrollInstructionsPresent = false;
         }
     }
 }
